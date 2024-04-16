0ed4b2f72dfaf22dc057948e6a0e949fa5a347b0
kento0810 <eh210036@meiji.ac.jp>
2024-04-16 18:32:21 +0900

    Add 音楽共有アプリ投稿詳細画面の実装

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,11 +7,17 @@ use App\Models\Post;
 use App\Models\Category;
 use App\Models\Comment;
 use App\Models\User;
+use Cloudinary;
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
         return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
+    }
+    
+    public function show(Post $post)
+    {
+        return view ('posts/show')->with(['post' => $post]);
     }
 }
